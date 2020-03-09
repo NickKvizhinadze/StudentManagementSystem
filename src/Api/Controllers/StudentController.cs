c38b7ea8f35111da83684eaf3295ebefc9497b41
Nick Kvizhinadze <nickkvizhinadze@gmail.com>
2020-03-09 15:12:36 +0400

    Refactored Students Create and get

--- src/Api/Controllers/StudentController.cs
+++ src/Api/Controllers/StudentController.cs
@@ -47,7 +47,7 @@ namespace Api.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create([FromBody] StudentDto dto)
+        public IActionResult Register([FromBody] NewStudentDto dto)
         {
             var student = new Student(dto.Name, dto.Email);
 
@@ -70,7 +70,7 @@ namespace Api.Controllers
         }
 
         [HttpDelete("{id}")]
-        public IActionResult Delete(long id)
+        public IActionResult UnRegister(long id)
         {
             Student student = _studentRepository.GetById(id);
             if (student == null)
